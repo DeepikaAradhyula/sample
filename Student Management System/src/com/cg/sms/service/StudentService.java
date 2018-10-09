67cc571c4ed573ce6924bda527057b9f4670dd35
DeepikaAradhyula <14wh1a0459@bvrithyderabad.edu.in>
2018-10-09 23:27:19 +0530

    Update StudentService.java

--- Student Management System/src/com/cg/sms/service/StudentService.java
+++ Student Management System/src/com/cg/sms/service/StudentService.java
@@ -10,6 +10,9 @@ import com.cg.sms.exception.StudentException;
 
 public class StudentService implements StudentInterface {
 
+	//create object for dao??
+	StudentInterface service=new StudentService();
+	
 	@Override
 	public void addStudentDetails(Student student) throws StudentException {
 		// TODO Auto-generated method stub
@@ -23,13 +26,23 @@ public class StudentService implements StudentInterface {
 	}
 
 	@Override
-	public boolean validateId(int id) throws StudentException {
+	public boolean validateDetails(int id,String name,String city,String phoneNo,String emailId){
+		if(service.validateId(id)&&service.validateName(name)&&service.validateCity(city)&&service.validateMobileNo(phoneNo)&&service.validateEmailId(emailId)){
+			return true;
+		}else{
+			throw new StudentException();
+		}
+		
+	}
+	
+	
+	private boolean validateId(int id) throws StudentException {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
-	public boolean validateName(String name) throws StudentException {
+	
+	private boolean validateName(String name) throws StudentException {
 		// TODO Auto-generated method stub
 		Pattern pattern = Pattern.compile( "[A-Z]{1}[a-z]");
 		Matcher matcher = pattern.matcher(name);
@@ -44,8 +57,8 @@ public class StudentService implements StudentInterface {
 		
 	}
 
-	@Override
-	public boolean validateCity(String city) throws StudentException {
+	
+	private boolean validateCity(String city) throws StudentException {
 		// TODO Auto-generated method stub
 		Pattern pattern = Pattern.compile( "[A-Z]{1}[a-z]");
 		Matcher matcher = pattern.matcher(city);
@@ -59,8 +72,8 @@ public class StudentService implements StudentInterface {
 		return true;		
 	}
 
-	@Override
-	public boolean validateMobileNo(String phoneNo) throws StudentException {
+	
+	private boolean validateMobileNo(String phoneNo) throws StudentException {
 		// TODO Auto-generated method stub
 		if(!phoneNo.matches("\\d[0-9]{10}")){
 			throw new StudentException("Moblie Number should contain 10 digits");			
@@ -69,8 +82,8 @@ public class StudentService implements StudentInterface {
 		
 	}
 
-	@Override
-	public boolean validateEmailId(String emailId) throws StudentException {
+	
+	private boolean validateEmailId(String emailId) throws StudentException {
 		// TODO Auto-generated method stub
 		if(!emailId.matches("^(.*)@(.*)$")){
 			
